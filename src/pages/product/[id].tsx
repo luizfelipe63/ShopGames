0590c743621c5ae2c4eaee2626e776e41a7e1b7f
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-02-24 19:57:25 -0300

    Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,61 @@
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import {useRouter} from 'next/router'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import { ImageProducts, ProductsContainer, ProductsDetails } from '../../styles/pages/products'
 
-export default function Product() {
-  const {query} = useRouter()
+
+interface ProductsProps {
+  products:{
+    id: string, 
+    name: string,
+    imageUrl: string,
+    price: string, 
+    descripton: string
+  }
+}
+
+export default function Product({products}: ProductsProps) {
+
 
   return (
     <ProductsContainer>
       <ImageProducts>
-        {/* <Image/> */}
+        <Image src={products.imageUrl} width={520} height={480} alt=''/>
       </ImageProducts>
       <ProductsDetails>
-        <h1>Camiseta X</h1>
-        <span>79,90</span>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, esse obcaecati ipsa officia quae, totam vero ducimus eius veritatis recusandae unde error eum blanditiis, eveniet itaque soluta voluptate sit possimus.</p>
+        <h1>{products.name}</h1>
+        <span>{products.price}</span>
+        <p>{products.descripton}</p>
         <button>Comprar agora</button>
       </ProductsDetails>
     </ProductsContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({params}) => {
+  const productId =  String(params.id)
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  
+  const price = product.default_price as Stripe.Price
+
+  return{
+    props:{
+      product:{
+        id: product.id,
+        imageUrl: product.images[0],
+        name: product.name, 
+        price: new Intl.NumberFormat('pt-BR',{
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount! / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1
+  }
 }
