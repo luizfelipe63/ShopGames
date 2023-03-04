dc5dadeae6ea3b62e74fbd48480b2482d8baaa2d
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-03-04 15:21:45 -0300

    Estrutura da página successo

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success(){
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+      <p>Uhuul Diego Fernandes, sua Camiseta Beyond the Limits já está a caminho da sua casa. </p>
+      <Link href='/'>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
 } 
